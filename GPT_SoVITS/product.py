c4926e7922af036f871df8086d88d2ea3e850e16
ruokbb <ruokbb@qq.com>
2024-01-30 18:38:51 +0800

    [init]

---
+++ GPT_SoVITS/product.py
@@ -0,0 +1,127 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time: 2024/1/29 17:13
+@Author: shiqixin.set
+@File: product.py
+@Software: PyCharm
+@desc: 
+"""
+
+from time import time as ttime
+import numpy as np
+import torch
+from model.model_loading import model_data, is_half, device,ssl_model, dict_language, tokenizer, bert_model, hz
+import librosa
+from text import cleaned_text_to_sequence
+from text.cleaner import clean_text
+from module.mel_processing import spectrogram_torch
+from tools.my_utils import load_audio
+
+
+def get_spepc(hps, filename):
+    audio = load_audio(filename, int(hps.data.sampling_rate))
+    audio = torch.FloatTensor(audio)
+    audio_norm = audio
+    audio_norm = audio_norm.unsqueeze(0)
+    spec = spectrogram_torch(audio_norm, hps.data.filter_length, hps.data.sampling_rate, hps.data.hop_length,
+                             hps.data.win_length, center=False)
+    return spec
+
+def get_bert_feature(text, word2ph):
+    with torch.no_grad():
+        inputs = tokenizer(text, return_tensors="pt")
+        for i in inputs:
+            inputs[i] = inputs[i].to(device)  #####输入是long不用管精度问题，精度随bert_model
+        res = bert_model(**inputs, output_hidden_states=True)
+        res = torch.cat(res["hidden_states"][-3:-2], -1)[0].cpu()[1:-1]
+    assert len(word2ph) == len(text)
+    phone_level_feature = []
+    for i in range(len(word2ph)):
+        repeat_feature = res[i].repeat(word2ph[i], 1)
+        phone_level_feature.append(repeat_feature)
+    phone_level_feature = torch.cat(phone_level_feature, dim=0)
+    # if(is_half==True):phone_level_feature=phone_level_feature.half()
+    return phone_level_feature.T
+
+def get_tts_wav(ref_wav_path, prompt_text, prompt_language, text, text_language, model_name):
+    hps = model_data[model_name]["hps"]
+    vq_model = model_data[model_name]["vq_model"]
+    t2s_model = model_data[model_name]["t2s_model"]
+    max_sec = model_data[model_name]["max_sec"]
+    top_k = model_data[model_name]["top_k"]
+
+    t0 = ttime()
+    prompt_text = prompt_text.strip("\n")
+    prompt_language, text = prompt_language, text.strip("\n")
+    zero_wav = np.zeros(int(hps.data.sampling_rate * 0.3), dtype=np.float16 if is_half == True else np.float32)
+    with torch.no_grad():
+        wav16k, sr = librosa.load(ref_wav_path, sr=16000)
+        wav16k = torch.from_numpy(wav16k)
+        zero_wav_torch = torch.from_numpy(zero_wav)
+        if (is_half == True):
+            wav16k = wav16k.half().to(device)
+            zero_wav_torch = zero_wav_torch.half().to(device)
+        else:
+            wav16k = wav16k.to(device)
+            zero_wav_torch = zero_wav_torch.to(device)
+        wav16k = torch.cat([wav16k, zero_wav_torch])
+        ssl_content = ssl_model.model(wav16k.unsqueeze(0))["last_hidden_state"].transpose(1, 2)  # .float()
+        codes = vq_model.extract_latent(ssl_content)
+        prompt_semantic = codes[0, 0]
+    t1 = ttime()
+    prompt_language = dict_language[prompt_language]
+    text_language = dict_language[text_language]
+    phones1, word2ph1, norm_text1 = clean_text(prompt_text, prompt_language)
+    phones1 = cleaned_text_to_sequence(phones1)
+    texts = text.split("\n")
+    audio_opt = []
+
+    for text in texts:
+        phones2, word2ph2, norm_text2 = clean_text(text, text_language)
+        phones2 = cleaned_text_to_sequence(phones2)
+        if (prompt_language == "zh"):
+            bert1 = get_bert_feature(norm_text1, word2ph1).to(device)
+        else:
+            bert1 = torch.zeros((1024, len(phones1)), dtype=torch.float16 if is_half == True else torch.float32).to(
+                device)
+        if (text_language == "zh"):
+            bert2 = get_bert_feature(norm_text2, word2ph2).to(device)
+        else:
+            bert2 = torch.zeros((1024, len(phones2))).to(bert1)
+        bert = torch.cat([bert1, bert2], 1)
+
+        all_phoneme_ids = torch.LongTensor(phones1 + phones2).to(device).unsqueeze(0)
+        bert = bert.to(device).unsqueeze(0)
+        all_phoneme_len = torch.tensor([all_phoneme_ids.shape[-1]]).to(device)
+        prompt = prompt_semantic.unsqueeze(0).to(device)
+        t2 = ttime()
+        with torch.no_grad():
+            # pred_semantic = t2s_model.model.infer(
+            pred_semantic, idx = t2s_model.model.infer_panel(
+                all_phoneme_ids,
+                all_phoneme_len,
+                prompt,
+                bert,
+                # prompt_phone_len=ph_offset,
+                top_k=top_k,
+                early_stop_num=hz * max_sec)
+        t3 = ttime()
+        # print(pred_semantic.shape,idx)
+        pred_semantic = pred_semantic[:, -idx:].unsqueeze(0)  # .unsqueeze(0)#mq要多unsqueeze一次
+        refer = get_spepc(hps, ref_wav_path)  # .to(device)
+        if (is_half == True):
+            refer = refer.half().to(device)
+        else:
+            refer = refer.to(device)
+        # audio = vq_model.decode(pred_semantic, all_phoneme_ids, refer).detach().cpu().numpy()[0, 0]
+        audio = \
+            vq_model.decode(pred_semantic, torch.LongTensor(phones2).to(device).unsqueeze(0),
+                            refer).detach().cpu().numpy()[
+                0, 0]  ###试试重建不带上prompt部分
+        audio_opt.append(audio)
+        audio_opt.append(zero_wav)
+        t4 = ttime()
+    print("%.3f\t%.3f\t%.3f\t%.3f" % (t1 - t0, t2 - t1, t3 - t2, t4 - t3))
+    yield hps.data.sampling_rate, (np.concatenate(audio_opt, 0) * 32768).astype(np.int16)
+
---
+++ voice_api.py
@@ -0,0 +1,164 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time: 2024/1/30 15:32
+@Author: shiqixin.set
+@File: voice_api.py
+@Software: PyCharm
+@desc: 
+"""
+from fastapi import File, UploadFile, Form, Header
+import json
+import os
+from fastapi import FastAPI, Request, HTTPException
+import uvicorn
+from starlette.responses import Response
+from fastapi.responses import StreamingResponse, JSONResponse
+from GPT_SoVITS.product import get_tts_wav
+import torch
+import soundfile as sf
+from io import BytesIO
+
+API_DIR = "api"
+USER_DATA_PATH = os.path.join(API_DIR, "user_data.json")
+USER_SOURCE_DIR_PATH = os.path.join(API_DIR, "user_source")
+os.makedirs(USER_SOURCE_DIR_PATH, exist_ok=True)
+
+app = FastAPI()
+
+
+def user_check(token):
+    """
+    判断user是否记录
+    :param token:
+    :return:
+    """
+    with open(USER_DATA_PATH, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        if token in data:
+            return True
+        else:
+            return False
+
+
+def handle(refer_wav_path, prompt_text, prompt_language, text, text_language, model_name="default"):
+    if (
+            refer_wav_path == "" or refer_wav_path is None
+            or prompt_text == "" or prompt_text is None
+            or prompt_language == "" or prompt_language is None
+    ):
+        return JSONResponse({"code": 400, "message": "未指定参考音频"}, status_code=400)
+
+    with torch.no_grad():
+        gen = get_tts_wav(
+            refer_wav_path, prompt_text, prompt_language, text, text_language, model_name
+        )
+        sampling_rate, audio_data = next(gen)
+
+    wav = BytesIO()
+    sf.write(wav, audio_data, sampling_rate, format="wav")
+    wav.seek(0)
+
+    torch.cuda.empty_cache()
+    try:
+        torch.mps.empty_cache()
+    except:
+        pass
+    return StreamingResponse(wav, media_type="audio/wav")
+
+
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+
+
+class VerifyTokenMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+            self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        if request.url.path == "/add_user":
+            return await call_next(request)
+
+        token = request.headers.get("token")
+        if token and user_check(token):
+            if not os.path.exists(os.path.join(USER_SOURCE_DIR_PATH, token)):
+                user_source_path = os.path.join(USER_SOURCE_DIR_PATH, token)
+                os.makedirs(user_source_path, exist_ok=True)
+                data_path = os.path.join(user_source_path, "data.json")
+                if not os.path.exists(data_path):
+                    with open(data_path, "w", encoding="utf-8") as f:
+                        json.dump({}, f)
+        else:
+            raise HTTPException(status_code=403, detail="token不存在")
+        return await call_next(request)
+
+
+app.add_middleware(VerifyTokenMiddleware)
+
+
+@app.get("/")
+async def tts_endpoint(
+        refer_name: str = None,
+        text: str = None,
+        text_language: str = None,
+        model_name: str = "default",
+        token: str = Header("default")
+):
+    data_path = os.path.join(USER_SOURCE_DIR_PATH, token, "data.json")
+    with open(data_path, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+        if refer_name not in data:
+            return JSONResponse({"code": 400, "message": "参考音频不存在，请重新上传"})
+        else:
+            refer_wav_path = os.path.join(USER_SOURCE_DIR_PATH, token, data[refer_name][0])
+            prompt_text = data[refer_name][1]
+            prompt_language = data[refer_name][2]
+            return handle(refer_wav_path, prompt_text, prompt_language, text, text_language, model_name)
+
+
+@app.post("/upload_refer")
+async def upload_refer(
+        file: UploadFile = File(...),
+        file_text: str = Form(...),
+        file_language: str = Form(...),
+        token: str = Header("default")
+):
+    file_type = file.filename.split(".")[-1]
+    file_type_list = ["opus", "flac", "webm", "wav", "m4a", "ogg", "oga", "mp3"]
+    if file_type not in file_type_list:
+        return JSONResponse({"code": 400, "message": "文件类型不符合:{}".format(file_type_list)})
+    # 保存视频
+    user_source_path = os.path.join(USER_SOURCE_DIR_PATH, token)
+    file_path = os.path.join(user_source_path, file.filename)
+    with open(file_path, "wb") as f:
+        contents = await file.read()
+        f.write(contents)
+
+    # 记录文字
+    user_source_data_path = os.path.join(user_source_path, "data.json")
+    with open(user_source_data_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    data[os.path.splitext(file.filename)[0]] = [file.filename, file_text, file_language]
+
+    with open(user_source_data_path, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+    return JSONResponse({"code": 0, "message": "Success"})
+
+
+@app.post("/add_user")
+async def control(request: Request):
+    json_post_raw = await request.json()
+    with open(USER_DATA_PATH, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+    data[json_post_raw.get("token")] = {
+        "name": json_post_raw.get("name"),
+        "level": json_post_raw.get("level")
+    }
+    with open(USER_DATA_PATH, "w", encoding="UTF-8") as f:
+        json.dump(data, f)
+    return JSONResponse({"code": 0, "message": "Success"})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=6553, workers=1)
